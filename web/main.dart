d39cb405db828eea9eb75c62899d19151f73874c
10154507114 <10154507114@ecnu.cn>
2017-10-19 01:03:14 +0800

    123

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,31 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import 'dart:math';
+
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  
+  var randomNumber;
+  randomNumber = new math.Random().nextInt(17);
+  var shuzi = new Random();
+ var list = List var name; 
+ var list = List var number;
+ name[1] = '韩庆慧' ;       number[1]=10154507101;
+  name[2] = '谢函' ;        number[2]=10154507102;
+  name[3]= '宋金阳' ;       number[3]=10154507103;
+  name[4] = '李佳璇' ;      number[4]=10154507104;
+  name[5] = '此人转专业' ;  number[5]=10154507105;
+  name[6] = '此人转专业' ;  number[6]=10154507106;
+  name[7] = '谢琪' ;        number[7]=10154507107;
+  name[8] = '此人转专业' ;   number[8]=10154507108;
+  name[9] = '杨芙蓉' ;      number[9]=10154507109;
+  name[10] = '荆泽宇' ;     number[10]=101545071010;
+  name[11] = '彭丽' ;       number[11]=101545071011;
+  name[12] = '黄大龙' ;     number[12]=101545071012;
+  name[13]= '李敏' ;        number[13]=10154507113;
+  name[14]= '刘冠群' ;      number[14]=10154507114;
+
+  //querySelector('#output').text = 'Your Dart app is running.';
+ querySelector("#sample_text_id").text='学号：101545071'+randomNumber.toString()+'  '+'姓名：'+'name'+randomNumber.toString(shuzi);
 }
