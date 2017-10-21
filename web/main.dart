af90b389589c83b6c5d8665c36e9f120f85a506f
10154507121 <32136315+10154507121@users.noreply.github.com>
2017-10-21 23:21:34 +0800

    Update main.dart

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,25 @@
-// Copyright (c) 2017, meflyup. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'dart:html';
+import 'dart:math' show Random;
 
-void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+int selectnum = 0;
+List name = ["stu1","stu2","stu3","stu4","stu5"];
+List stunum = ["10154507101","10154507102","10154507103","10154507104","10154507105"];
+InputElement selectstu;
+
+
+void main()
+{
+  querySelector("#click").onClick.listen(clicked);
+  selectstu = querySelector("#select");
+}
+
+void clicked(Event c)
+{
+  if(selectstu.value == "class")
+  {
+    Random select_number = new Random();
+    int number = select_number.nextInt(name.length);
+    querySelector("#select-name").text = name[number];
+    querySelector("#select-stunum").text = stunum[number];
+  }
 }
