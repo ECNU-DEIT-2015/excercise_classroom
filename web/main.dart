fb674d71caec6e08fe6d89c2607fe5686ab369ae
sherrytowne <10154507127@ecnu.cn>
2017-10-21 20:02:18 +0800

    10154507127,第一次代码作业

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,38 @@
-// Copyright (c) 2017, meflyup. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'dart:html';
+import 'dart:math' show Random;
+ButtonElement genButton;
+var m=0;
+class PirateName {
+  //想用class创建一个类，存储学生姓名和学号。
+  static final Random indexGen = new Random();
+  String _stuName;
+  int _id;
+  PirateName({String stuName}) {
+      _stuName = names[indexGen.nextInt(names.length)];
+  //    _id = ids[indexGen.nextInt(ids.length)];
+
+    }
+
+  String toString() => pirateName;
+
+  String get pirateName =>
+      _stuName.isEmpty ? '' : '$_stuName';
+
+  static final List names = [
+    'Anne-01', 'Mary-02' ,'Jackal-03', 'King-04','Tommy-05','Shirley-06','Flora-07','Michael-08','Laura-09','Queen-10'];
+//  static final List ids = ['01', '02' ,'03', '04','05','06','07','08','09','10'];
+
+}
+
+void setstuinfo(PirateName newName) {
+  querySelector('#studentName').text = newName.pirateName;
+}
+
+void checkstu(Event e) {
+  setstuinfo(new PirateName());
+}
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  genButton = querySelector('#clickButton');
+  genButton.onClick.listen(checkstu);
 }
