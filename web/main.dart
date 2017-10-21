f43e3c02409d90c177a1c79e0ad7e2035d18f025
10154507114 <10154507114@ecnu.cn>
2017-10-21 19:23:39 +0800

    '附加功能：1）界面排版：文字加

--- web/main.dart
+++ web/main.dart
@@ -3,38 +3,41 @@
 
 import 'dart:html';
 import 'dart:math' as math;
+var times= new List<int>();
 
 
 void main() {
   querySelector('#anniu')
-    ..text = '点击此处随机点名'
-    ..onClick.listen(reverseText);
+    ..id
+    ..onClick.listen(dianjishijian);
+    times.addAll([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
 }
-void reverseText(MouseEvent event)
-{ var idnumber=new math.Random().nextInt(37);
+void dianjishijian(MouseEvent event)
+{ var idnumber=new math.Random().nextInt(38);
   var studentname = new List<String>();
+  var eimformation = new List<String>();
 studentname.addAll([
-  '韩庆慧', 
-  '谢函',
+   '韩庆慧', 
+   '谢函',
    '宋金阳',
    '李佳璇',
-   '此人已转专业',
-   '此人已转专业',
+   '空',
+   '宋利',
    '谢琪',
-   '此人已转专业',
+   '张珑川',
    '杨芙蓉',
    '荆泽宇',
    '彭丽',
    '黄大龙',
    '李敏',
    '刘冠群',
-   '此人已转专业',
-   '此人已转专业',
+   '陈冠雄',
+   '郑素清',
    '林凌',
-   '此人已转专业',
+   '庄子凯',
    '颜欢',
-   '此人已转专业',
-   '此人已转专业',
+   '宗雨',
+   '彭茂源',
    '朋秋霞',
    '张思宁',
    '崔红洋',
@@ -43,18 +46,63 @@ studentname.addAll([
    '汤夏颖',
    '武美玖',
    '吴兵可',
-   '此人已转专业',
+   '祁馨禾',
    '任子佳',
    '苏颖晞',
    '宋婷袅',
    '姜宇轩',
-   '此人已转专业',
+   '刘思延',
    '周雨萌',
+   '徐晟炜',
+   '于潇雪',
+   '李庆泉']);
+   eimformation.addAll([
+   '慧慧是一个很积极向上的同学',
+   '谢函是一个很漂亮的小女孩',
+   '宋金阳主席哈哈哈哈',
+   '嘻嘻，佳璇是红不了的红人物',
+   '班级缺了一个学号啊哈哈',
    '此人已转专业',
-   '于潇雪']);
+   '谢琪呀，清悦口琴社社长',
+   '此人已转专业',
+   '芙蓉妹妹啊哈哈哈',
+   '荆泽宇呐，一位来自内蒙的内向同学',
+   '彭丽，壹幕工作室小伙伴',
+   '大龙，与室友同一个高中毕业的重庆小伙子',
+   '李敏，与大龙同一个高中毕业的室友',
+   '小群子，这就是我啦~',
+   '此人已转专业',
+   '此人已转专业',
+   '从不吃辣椒到变的比我都能吃辣的福建室友',
+   '此人已转专业',
+   '这学期经历过惊吓与惊喜的江西室友',
+   '此人已转专业',
+   '新晋学委大大',
+   '秋霞，大一时期的代理班长一枚',
+   '软萌软萌的思宁同学啊',
+   '红洋红洋红洋~',
+   '大学霸同学~美国交流ing',
+   '超厉害啥都知道的雨辰~',
+   '小仙女，超厉害，软萌妹子，惜晨社长',
+   '九妹九妹，漂亮的妹妹~气质绝佳！',
+   '可可可可可可可可！可可~',
+   '此人已转专业',
+   '小佳佳~云南的妹子舞蹈都很厉害~',
+   '文体中心的重把手苏苏~',
+   '嘻嘻~系嫂？',
+   '公认大佬！',
+   '此人已转专业',
+   '早上吃什么，中午吃什么，晚上吃什么？',
+   '此人已转专业',
+   '压轴班长大大！美丽全能的小雪儿~',
+   '此人已转专业'
+   ]);
 if(idnumber>=10){querySelector("#studentid").text='学号：  101545071'+(idnumber+1).toString();}
   else {querySelector("#studentid").text='学号：  1015450710'+(idnumber+1).toString();}
 querySelector("#name").text='姓名：'+studentname[idnumber];
+querySelector("#evaluate").text='了解一下这位同学的情况吧:      '+eimformation[idnumber];
+times[idnumber]++;
+querySelector("#timestotal").text='该同学在此堂课上已被点名：'+times[idnumber].toString()+'次';
 }
   
 
