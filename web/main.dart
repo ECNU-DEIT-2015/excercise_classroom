bc0e95fe0bc31586dd19da5ccb56b9567e641d98
鹿林keys <keys>
2017-10-21 23:49:24 +0800

    添加作业

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,27 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import 'dart:math' show Random;
+ButtonElement genButton;
+
+class Student{
+  static final Random indexGen = new Random();
+  final int id;
+  String name;
+  Student(this.id,this.name);
+  String toString()=>'$id,$name';
+List<Student> students=[new Student(1,'Apple'),new Student(2,'Banana'),new Student(3,'Orange'),new Student(4,'Watermelon'),
+new Student(5,'Pear'),new Student(6,'Lemon'),new Student(7,'Mango')];
+}
+
+
+
+void Click(Event e){
+ var number=new math.Random().nextInt(10);
+  querySelector('#name').text = Student(number,name);
+}
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  querySelector('clickButton').text = '点名小程序.';
+  ..onClick.listen(Click)
 }
