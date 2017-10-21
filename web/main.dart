bc16ab6712509fc5cf6b3be63a5f7c1575964147
LemonCui <10154507124.ecnu.cn>
2017-10-21 22:48:13 +0800

    10154507124,第一次代码作业

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,32 @@
-// Copyright (c) 2017, meflyup. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'dart:html';
+import 'dart:math' show Random;
+
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+
+  querySelector('#sample_text_id')
+    ..text = '开始点名!'
+    ..onClick.listen(makeCall);
 }
+
+
+  void makeCall(MouseEvent event) {
+    var name = [
+// Keys      Values
+    '1.'     '小明',
+    '2.'     '小红',
+    '3.'     '小城',
+    '4.'     '小洋',
+    '5.'     '小吕',
+    '6.'     '小心',
+    '7.'     '小例',
+    '8.'     '小系',
+    '9.'     '小力',
+    '10.'     '小曲'
+    ];
+
+
+    var ad=new Random();
+    querySelector('#sample_text_id').text = name[ad.nextInt(name.length)];
+  }
+
