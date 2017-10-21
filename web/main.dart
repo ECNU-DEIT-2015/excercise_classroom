4f154b39b4493619b868abbea091649c5eb9e62d
JzyTvT <32353171+JzyTvT@users.noreply.github.com>
2017-10-21 23:19:05 +0800

    Update main.dart

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,19 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import 'dart:math';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+
+  Map huamingce = new Map();
+huamingce[0] = '荆泽宇：10154507110';
+huamingce[1] = '黄大龙：10154507112';
+huamingce[2] = '茂茂：10154507123';
+
+  querySelector('#DML').
+  onClick.listen((MouseEvent ) {
+  var suiji = new Random();
+  querySelector('#output').text = huamingce[suiji.nextInt(3)];
+  
+});
 }
