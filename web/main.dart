891c088ab0a4febec4b7d1b89923f9c8ff1ae9eb
10154507134 <32149520+10154507134@users.noreply.github.com>
2017-10-21 08:37:45 -0500

    Update main.dart

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,16 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import 'dart:math' as math;
+import 'dart:math' show Random;
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  querySelector('#button').onClick.listen(reverseText);
+}
+
+void reverseText(Event e) {
+  Random Rand = new Random();
+  int randnumber = Rand.nextInt(3);
+  var nobleGases = ["学生1","学生2","学生3"];
+  querySelector('#sample_text_id').text = nobleGases[randnumber];
 }
