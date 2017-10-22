c6d452f1063a72ff29e852d123e6704edca9b021
meflyup <meflyup@gmail.com>
2017-10-22 16:04:18 +0800

    本次评分以及代码注释

--- web/main.dart
+++ web/main.dart
@@ -2,9 +2,10 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
-import 'dart:math' show Random;
+import 'dart:math' ;
 ButtonElement genButton;
 
+//很好，尝试使用类来做。加油！
 class Student{
   static final Random indexGen = new Random();
   final int id;
@@ -18,6 +19,7 @@ new Student(5,'Pear'),new Student(6,'Lemon'),new Student(7,'Mango')];
 
 
 void Click(Event e){
+//对包的使用有误，想想：quesrySelector怎么不用html.querySelecto？
  var number=new math.Random().nextInt(10);
   querySelector('#name').text = Student(number,name);
 }
@@ -25,4 +27,5 @@ void Click(Event e){
 void main() {
   querySelector('clickButton').text = '点名小程序.';
   ..onClick.listen(Click)
+  //..onClick的用法有误，请查看案例。dart语句语法有误，请查看案例
 }
