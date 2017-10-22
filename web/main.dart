d7dc69fbf3512117cec532375fbcbe9a9a6192f6
Quinn Norris <sei_qnz2015@126.com>
2017-10-22 16:19:28 +0800

    点名程序

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,31 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import "dart:math";
 
 void main() {
   querySelector('#output').text = 'Your Dart app is running.';
+
+  var startBtn = query(".class_but");
+  startBtn.onClick.listen((e) {
+    start();
+  });
+}
+
+start() {
+  var nobleGases = new Map<String, String>();
+  nobleGases["2015001"] = "A";
+  nobleGases["2015002"] = "B";
+  nobleGases["2015003"] = "C";
+  nobleGases["2015004"] = "D";
+  nobleGases["2015005"] = "E";
+  nobleGases["2015006"] = "F";
+  nobleGases["2015007"] = "G";
+  nobleGases["2015008"] = "H";
+  nobleGases["2015009"] = "I";
+  var randomNum = new Random().nextInt(9);
+  var number = "201500" + randomNum.toString();
+  var name = nobleGases[number];
+  query(".name").innerHtml = "$name";
+  query(".number").innerHtml = "$number";
 }
