5c846ea0d67ee22704b24aea03047b51f9f7d1e2
10154507102 <10154507102@stu.ecnu.edu.cn>
2017-11-02 22:21:56 +0800

    '增加了新功能和背景‘

--- web/main.dart
+++ web/main.dart
@@ -1,17 +1,37 @@
 // Copyright (c) 2017, meflyup. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
 
 // Copyright (c) 2017, meflyup. All rights reserved. Use of this source code 
 // is governed by a BSD-style license that can be found in the LICENSE file. 
-import 'dart:html'; 
+
+import 'dart:html';
 import 'dart:math' as math; 
+UListElement DoList;
+ButtonElement deleteAll;
+String stuname;
+
 void main() { 
 querySelector('#sample_text_id') 
 ..text = '点击点名' 
 ..onClick.listen(reverseText); 
+
+DoList = querySelector('#list_id');  
+DoList.onChange.listen(addToDoItem);
+deleteAll= querySelector('#delete-all'); 
+deleteAll.onClick.listen(deleteAllElements);
+
 } 
+
+void addToDoItem(Event e){
+ var ToDo = new LIElement();
+ToDo.text=stuname;
+DoList.children.add(ToDo); 
+}
+void deleteAllElements(Event e) {
+  DoList.children.clear();
+}
+
 void reverseText(MouseEvent event) { 
 var randomNumber = new math.Random().nextInt(16); 
 var stuNames = { 
@@ -19,5 +39,8 @@ var stuNames = {
 }; 
 querySelector("#sample_text_id").text='学号：'+randomNumber.toString() 
 +' '+' '+' '+'姓名：'+stuNames[randomNumber]; 
-} 
 
+stuname=stuNames[randomNumber];
+addToDoItem(event);
+
+} 
